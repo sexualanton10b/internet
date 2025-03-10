2482c369d162e598e9450a55bebadeabe5b6c1b0
RACER0617 <116666344+RACER0617@users.noreply.github.com>
2025-03-10 14:42:48 +0300

    Добавлен метод emulate для каждого класса, который генерирует случайные значения в разных диапазонах (например, для SignalLamp — 0 или 1, как для состояния вкл/выкл). Метод connect теперь вызывает emulate, выводит сообщение и возвращает данные в JSON-формате. Аргумент source из предыдущей версии убран, так как он не нужен для мониторинга.

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import abc
-
+import random
+import json
 
 class Device(abc.ABC):
     def __init__(self, name):
@@ -20,7 +21,12 @@ class Robot(Device):
 
     def connect(self, source):
         super().connect()
-        return ('Robot connection to ' + source + ' success')
+        self.emulate()
+        print(f'Connection with {self.name} success, new value is {self.value}')
+        return json.dumps({'value': self.value})
+
+    def emulate(self):
+        self.value = random.randint(15, 25)
 
 
 class SmartCamera(Device):
@@ -32,8 +38,12 @@ class SmartCamera(Device):
 
     def connect(self, source):
         super().connect()
-        return ('SmartCamera connection to ' + source + ' success')
+        self.emulate()
+        print(f'Connection with {self.name} success, new value is {self.value}')
+        return json.dumps({'value': self.value})
 
+    def emulate(self):
+        self.value = random.randint(0, 100)
 
 class SignalLamp(Device):
     def __init__(self, unit, name):
@@ -44,16 +54,20 @@ class SignalLamp(Device):
 
     def connect(self, source):
         super().connect()
-        return ('SignalLamp connection to ' + source + ' success')
+        self.emulate()
+        print(f'Connection with {self.name} success, new value is {self.value}')
+        return json.dumps({'value': self.value})
+
+    def emulate(self):
+        self.value = random.randint(0, 1) # 0 или 1, как для лампы (вкл/выкл)
 
 
 class Terminal(Device):
-    def __init__(self, unit, name):
-        super().__init__(name)
-        self.unit = unit
-        self.value = 0
-        print('terminal has create')
-
-    def connect(self, source):
+    def connect(self):
         super().connect()
-        return ('Terminal connection to ' + source + ' success')
+        self.emulate()
+        print(f'Connection with {self.name} success, new value is {self.value}')
+        return json.dumps({'value': self.value})
+
+    def emulate(self):
+        self.value = random.randint(-10, 10)
